eb6a158079b95fada939e26149bafea90e4ec306
Grant Emerson <grant.alex.emerson@gmail.com>
2022-02-20 17:48:37 -0600

    bring in bootstrap fa and bootswatch theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './bootstrap.min.css'
 import App from './App';
+import './index.css'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
